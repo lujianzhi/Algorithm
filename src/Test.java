3be86041d17ae399ea07ae6a02fdbb41f2a53e86
lujianzhi <lujianzhixiaoyao@163.com>
2017-03-27 11:50:51 +0800

    Hello Algorithm

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Ian.Lu on 2017/3/27.
+ * Project : Algorithm
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello Algorithm");
+    }
+}
